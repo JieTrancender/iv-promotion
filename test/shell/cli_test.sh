c4b3e7dd2cfc6af764e5c5c7f877a7819e8fd07f
Jason <jie-email@jie-trancender.org>
2020-12-04 20:04:44 +0800

    debug service

--- test/shell/cli_test.sh
+++ test/shell/cli_test.sh
@@ -1,10 +1,9 @@
 set -ex
 
-/usr/bin/docker ps
-
-etcdctl user add root --new-user-password 123456
-etcdctl role add root
-etcdctl user grant-role root root
-etcdctl auth enable
+containerId=$(docker ps |grep etcd |awk '{print $1}')
+/usr/bin/docker exec -it $(containerId) /bin/sh -c 'etcdctl user add root --new-user-password 123456'
+/usr/bin/docker exec -it $(containerId) /bin/sh -c 'etcdctl role add root'
+/usr/bin/docker exec -it $(containerId) /bin/sh -c 'etcdctl user grant-role root root'
+/usr/bin/docker exec -it $(containerId) /bin/sh -c 'etcdctl auth enable'
 
 curl -ig 'localhost:2379/ping'

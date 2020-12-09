7c427204abb3511e3b62a3c1cc48794eb2f3593b
Jason <jie-email@jie-trancender.org>
2020-12-09 22:07:57 +0800

    optimize multi generation

--- apps/multiGenerate/main.go
+++ apps/multiGenerate/main.go
@@ -3,40 +3,65 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
-func generateIntA() chan int {
-	ch := make(chan int, 10)
+func generateIntA(done chan struct{}) chan int {
+	ch := make(chan int, 5)
 	go func() {
-		for {
-			ch <- rand.Int()
-		}
-	}()
-
-	return ch
-}
-
-func generateIntB() chan int {
-	ch := make(chan int, 10)
-	go func() {
-		for {
-			ch <- rand.Int()
-		}
-	}()
-
-	return ch
-}
-
-func generateInt() chan int {
-	ch := make(chan int, 20)
-	go func() {
+	Lable:
 		for {
 			select {
-			case ch <- <-generateIntA():
-			case ch <- <-generateIntB():
+			case ch <- rand.Int():
+			case <-done:
+				break Lable
 			}
 		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+func generateIntB(done chan struct{}) chan int {
+	ch := make(chan int, 5)
+	go func() {
+	Lable:
+		for {
+			select {
+			case ch <- rand.Int():
+			case <-done:
+				break Lable
+			}
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
+func generateInt(done chan struct{}) chan int {
+	ch := make(chan int)
+	seed := make(chan struct{})
+	go func() {
+		randA := generateIntA(seed)
+		randB := generateIntB(seed)
+	Lable:
+		for {
+			select {
+			case ch <- <-randA:
+			case ch <- <-randB:
+			case <-done:
+				seed <- struct{}{}
+				seed <- struct{}{}
+				break Lable
+			}
+		}
+
+		close(ch)
 	}()
 
 	return ch
@@ -45,9 +70,19 @@ func generateInt() chan int {
 func main() {
 	rand.Seed(time.Now().Unix())
 
-	ch := generateInt()
+	done := make(chan struct{})
+	ch := generateInt(done)
 
-	for i := 0; i < 10000; i++ {
+	time.Sleep(1 * time.Second)
+	fmt.Println("NumGoroutine", runtime.NumGoroutine())
+
+	for i := 0; i < 10; i++ {
 		fmt.Println(<-ch)
 	}
+
+	// 通知生成器停止生产
+	done <- struct{}{}
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("stop generate", runtime.NumGoroutine())
 }
